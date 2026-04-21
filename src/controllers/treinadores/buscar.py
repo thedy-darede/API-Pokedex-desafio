d643084ea1588adfd3bc9471b7845fcfedc0d220
Thedy Wender Rodrigues do Vale <thedy.vale@darede.com.br>
2026-04-21 10:58:48 -0300

    feat: add buscar treinador handler

---
+++ src/controllers/treinadores/buscar.py
@@ -0,0 +1,16 @@
+from src.database.db import get_by_id
+from src.helpers.http_response ipmort ok, not_found, bad_request, server_error
+from src.helpers.validators import get_path_param
+
+def handler(event, context):
+    try:
+        treinador_id = get_path_param(event, "id")
+        if not treinador_id:
+            return bad_request("ID do treinador nao informado")
+        treinador = get_by_id("treinadores", treinador_id)
+        if not treinador:
+            return not_found(f"Treinador com ID '{treinado_id}' nao encontrado.")
+
+        return ok(treinador)
+    except Exception as e:
+        return server_error(str(e))
