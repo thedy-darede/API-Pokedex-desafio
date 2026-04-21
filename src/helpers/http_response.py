f7ced34e663d420a14c0737b7f2a5ae53a710f28
Thedy Wender Rodrigues do Vale <thedy.vale@darede.com.br>
2026-04-21 09:42:58 -0300

    feat: add http_response helper

---
+++ src/helpers/http_response.py
@@ -0,0 +1,37 @@
+import json
+
+
+def _build_response(status_code: int, body) -> dict:
+    return {
+        "statusCode": status_code,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+        },
+        "body": json.dumps(body, ensure_ascii=False),
+    }
+
+
+def ok(body) -> dict:
+    return _build_response(200, body)
+
+
+def created(body) -> dict:
+    return _build_response(201, body)
+
+
+def bad_request(message: str) -> dict:
+    return _build_response(400, {"erro": message})
+
+
+def not_found(message: str = "Recurso nao encontrado") -> dict:
+    return _build_response(404, {"erro": message})
+
+
+def unprocessable(message: str) -> dict:
+    return _build_response(422, {"erro": message})
+
+
+def server_error(message: str = "Erro interno do servidor") -> dict:
+    return _build_response(500, {"erro": message})
+
